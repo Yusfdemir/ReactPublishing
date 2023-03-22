0f9d8f53df0babcc7e150e3f625e9b311bcebbf0
Yusfdemir <69212816+Yusfdemir@users.noreply.github.com>
2023-03-22 15:32:37 +0300

    developui eklendi

---
+++ developui/example/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Example,Button } from 'developui'
+import 'developui/dist/index.css'
+
+const App = () => {
+  return(
+    <>
+     <Button text="primary" />
+     <Button text="dashed" />
+     <Button text="link" />
+     <Button text="text" />
+   
+    </>
+  )
+}
+
+export default App
---
+++ developui/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export const Button=(props)=>{
+  return(
+    <button {...props} className={`${styles.btn} ${styles[props.text]}`}>{`${props.text} Button`}</button>
+  )
+}
+
